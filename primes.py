15a859f39e627a8553a7cf07a078411c1ef5bba2
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2022-10-01 16:25:55 +0100

    Raise value error, calculate primes list with sqrt

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
     list = []
+    if (number_of_primes <= 0):
+        raise ValueError('Only input numbers greater than 0')
+    num = 2
+    list.append(num)
+    while(len(list) < number_of_primes):
+        counter = 0
+        num+=1
+        for i in range(2,int(math.sqrt(num)+1)):
+            if(num % i == 0):
+                counter+=1
+                break
+        if(counter == 0):
+            list.append(num)
+    
     return list
+
+
